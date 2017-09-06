f4945448d10f993e73fc7e896c27150565c024ed
Vlad Kurdzesau <vkurdesov@marketvision.com>
2017-09-06 05:35:07 -0500

    initial checking of project structure

---
+++ src/Cronus.Transport.AzureServiceBus/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cronus.Transport.AzureServiceBus
+{
+    public class Class1
+    {
+    }
+}
